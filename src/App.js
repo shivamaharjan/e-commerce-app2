2acff3ba376c504622bc9ed3dd8cbfb410b0fc89
Shiva <seebaw.m@gmail.com>
2024-04-20 17:59:04 +1000

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route></Routes>
+    </div>
   );
 }
 
